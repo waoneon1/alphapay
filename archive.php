4e5b890387b6dad8b10237b7a57237261a0959d6
waone <waone.on1@gmail.com>
2019-02-19 18:32:16 +0700

    blog & blog archive

--- archive.php
+++ archive.php
@@ -10,44 +10,142 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<?php $arc_nav = get_field('archive_navigation', 'option'); ?>
+<!-- archive header nav -->
+<div class="alp-archive--header">
+	<div class="container">
+		<ul>
+			<?php foreach ($arc_nav as $key => $term): ?>
+				<li>
+					<a href="<?php echo get_category_link($term->term_id) ?>">
+						<?php echo $term->name ?>
+					</a>
+				</li>
+			<?php endforeach ?>
+		</ul>
+	</div>
+</div>
 
-		<?php if ( have_posts() ) : ?>
+<!-- Banner Carosel -->
+<div class="alp-banner alp-sec-margin">
+	<div class="alp-banner--bg container">
+		
+		<div id = "carouselwithIndicators" class = "carousel slide" data-ride = "carousel">
+            <ol class = "carousel-indicators">
+               <li data-target = "#carouselExampleIndicators" data-slide-to = "0" class = "active"></li>
+               <li data-target = "#carouselExampleIndicators" data-slide-to = "1"></li>
+               <li data-target = "#carouselExampleIndicators" data-slide-to = "2s"></li>
+            </ol>
+            
+            <div class =" carousel-inner">
+               <div class = "carousel-item active">
+                  <img class = "d-block w-100" 
+                     src = "https://www.tutorialspoint.com/bootstrap/images/slide1.png" 
+                     alt = "First slide">
+               </div>
+               
+               <div class = "carousel-item">
+                  <img class = "d-block w-100" 
+                     src = "https://www.tutorialspoint.com/bootstrap/images/slide2.png" 
+                     alt = "Second slide">
+               </div>
+               <div class = "carousel-item">
+                  <img class = "d-block w-100" 
+                     src = "https://www.tutorialspoint.com/bootstrap/images/slide3.png" 
+                     alt = "Third slide">
+               </div>
+            </div>
+            
+            <a class = "carousel-control-prev" href = "#carouselwithIndicators" role = "button" data-slide = "prev">
+               <span class = "carousel-control-prev-icon" aria-hidden = "true"></span>
+               <span class = "sr-only">Previous</span>
+            </a>
+            
+            <a class = "carousel-control-next" href = "#carouselwithIndicators" role = "button" data-slide = "next">
+               <span class = "carousel-control-next-icon" aria-hidden = "true"></span>
+               <span class = "sr-only">Next</span>
+            </a>
+        </div>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+	</div>
+</div>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+<div class="alp-wrapper">
 
-			endwhile;
+	<div class="alp-content container">
+		<div class="row">
+			<div class="col-md-8 col-12">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-12">
+						<div class="alp-card">
+							<img src="<?php echo get_template_directory_uri() . '/assets/img/contentseo1.png' ?>">
+							<div class="alp-card--content">
+								<h2>How to Manage Your Audience in AlphaPay</h2>
+				 				<p>Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do </p>	
+				 				<a href="#" class="alp-card--btn">Baca Selanjutnya</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 col-12">
+						<div class="alp-card">
+							<img src="<?php echo get_template_directory_uri() . '/assets/img/contentseo1.png' ?>">
+							<div class="alp-card--content">
+								<h2>How to Manage Your Audience in AlphaPay</h2>
+					 			<p>Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do </p>		
+					 			<a href="#" class="alp-card--btn">Baca Selanjutnya</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 col-12">
+						<div class="alp-card">
+							<img src="<?php echo get_template_directory_uri() . '/assets/img/contentseo1.png' ?>">
+							<div class="alp-card--content">
+								<h2>How to Manage Your Audience in AlphaPay</h2>
+				 				<p>Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do </p>	
+				 				<a href="#" class="alp-card--btn">Baca Selanjutnya</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 col-12">
+						<div class="alp-card">
+							<img src="<?php echo get_template_directory_uri() . '/assets/img/contentseo1.png' ?>">
+							<div class="alp-card--content">
+								<h2>How to Manage Your Audience in AlphaPay</h2>
+					 			<p>Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do </p>		
+					 			<a href="#" class="alp-card--btn">Baca Selanjutnya</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 col-12">
+						<div class="alp-card">
+							<img src="<?php echo get_template_directory_uri() . '/assets/img/contentseo1.png' ?>">
+							<div class="alp-card--content">
+								<h2>How to Manage Your Audience in AlphaPay</h2>
+				 				<p>Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do </p>	
+				 				<a href="#" class="alp-card--btn">Baca Selanjutnya</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 col-12">
+						<div class="alp-card">
+							<img src="<?php echo get_template_directory_uri() . '/assets/img/contentseo1.png' ?>">
+							<div class="alp-card--content">
+								<h2>How to Manage Your Audience in AlphaPay</h2>
+					 			<p>Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do </p>		
+					 			<a href="#" class="alp-card--btn">Baca Selanjutnya</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 col-12">
+				<?php get_sidebar() ?>
+			</div>
+		</div>
+	</div>
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</div>
 
 <?php
-get_sidebar();
 get_footer();
--- single.php
+++ single.php
@@ -10,33 +10,99 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+
+<?php $arc_nav = get_field('archive_navigation', 'option'); ?>
+<!-- archive header nav -->
+<div class="alp-archive--header">
+	<div class="container">
+		<ul>
+			<?php foreach ($arc_nav as $key => $term): ?>
+				<li>
+					<a href="<?php echo get_category_link($term->term_id) ?>">
+						<?php echo $term->name ?>
+					</a>
+				</li>
+			<?php endforeach ?>
+		</ul>
+	</div>
+</div>
+
+<!-- Banner Carosel -->
+<div class="alp-banner alp-sec-margin">
+	<div class="alp-banner--bg container">
 		
-		<?php 
-		echo "<pre>";
-			print_r(get_field('sejarah'));
-		?>
+		<div id = "carouselwithIndicators" class = "carousel slide" data-ride = "carousel">
+            <ol class = "carousel-indicators">
+               <li data-target = "#carouselExampleIndicators" data-slide-to = "0" class = "active"></li>
+               <li data-target = "#carouselExampleIndicators" data-slide-to = "1"></li>
+               <li data-target = "#carouselExampleIndicators" data-slide-to = "2s"></li>
+            </ol>
+            
+            <div class =" carousel-inner">
+               <div class = "carousel-item active">
+                  <img class = "d-block w-100" 
+                     src = "https://www.tutorialspoint.com/bootstrap/images/slide1.png" 
+                     alt = "First slide">
+               </div>
+               
+               <div class = "carousel-item">
+                  <img class = "d-block w-100" 
+                     src = "https://www.tutorialspoint.com/bootstrap/images/slide2.png" 
+                     alt = "Second slide">
+               </div>
+               <div class = "carousel-item">
+                  <img class = "d-block w-100" 
+                     src = "https://www.tutorialspoint.com/bootstrap/images/slide3.png" 
+                     alt = "Third slide">
+               </div>
+            </div>
+            
+            <a class = "carousel-control-prev" href = "#carouselwithIndicators" role = "button" data-slide = "prev">
+               <span class = "carousel-control-prev-icon" aria-hidden = "true"></span>
+               <span class = "sr-only">Previous</span>
+            </a>
+            
+            <a class = "carousel-control-next" href = "#carouselwithIndicators" role = "button" data-slide = "next">
+               <span class = "carousel-control-next-icon" aria-hidden = "true"></span>
+               <span class = "sr-only">Next</span>
+            </a>
+        </div>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+	</div>
+</div>
 
-			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+<div class="alp-wrapper">
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+	<div class="alp-content container">
+		<div class="row">
+			<div class="col-md-8 col-12">
+				<!-- Content Here -->
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-		endwhile; // End of the loop.
-		?>
+					get_template_part( 'template-parts/content', get_post_type() );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					the_post_navigation();
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+				endwhile; // End of the loop.
+				?>
+			</div>
+			<div class="col-md-4 col-12">
+				<?php get_sidebar() ?>
+			</div>
+		</div>
+	</div>
+
+</div>
 
 <?php
 get_sidebar();
 get_footer();
+

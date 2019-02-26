f2cc590bb11142aefa49fe8483aec5b724010082
waone <waone.on1@gmail.com>
2019-02-26 19:43:27 +0700

    subscribe hardcoded

--- template-parts/flex-subscribe.php
+++ template-parts/flex-subscribe.php
@@ -2,8 +2,8 @@
 <div class="alp-subsc row alp-section">
 	<div class="col-md-6 col-12 alp-subsc--left alp-row">
 		<div>
-			<h2>Mau Info Terbaru dari AlphaPay?</h2>
-			<p>Daftarkan email dan nomor handphone Anda pada kolom berikut untuk menerima berbagai info, tips bisnis, hingga promo menarik dari AlphaPay.</p>
+			<h2>Mau Dapat Info Terbaru dari AlphaPay?</h2>
+			<p>Daftarkan email dan nomor hp Anda pada kolom berikut untuk menerima berbagai info, tips bisnis, dan promo menarik dari AlphaPay.</p>
 			<div class="alp-subsc--form alp-form">
 
 				<div class="field">

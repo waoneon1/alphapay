94faca06954b5c3474c2b02c03343ff96c2718bc
waone <waone.on1@gmail.com>
2019-03-02 00:10:22 +0700

    ilangin titik di subscrb

--- template-parts/flex-subscribe.php
+++ template-parts/flex-subscribe.php
@@ -3,7 +3,7 @@
 	<div class="col-md-6 col-12 alp-subsc--left alp-row">
 		<div>
 			<h2>Mau Dapat Info Terbaru dari AlphaPay?</h2>
-			<p>Daftarkan email dan nomor handphone Anda pada kolom berikut untuk menerima berbagai info, tips bisnis, dan promo menarik dari AlphaPay.</p>
+			<p>Daftarkan email dan nomor handphone Anda pada kolom berikut untuk menerima berbagai info, tips bisnis, dan promo menarik dari AlphaPay</p>
 			<div class="alp-subsc--form alp-form">
 				<?php echo do_shortcode($section['contact_form_7']) ?>
 			</div>

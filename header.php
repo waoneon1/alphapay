23ac7aac65064d62bab5e6da154a45c9a9721fb7
waone <waone.on1@gmail.com>
2019-03-22 14:59:45 +0700

    header google tag manager

--- header.php
+++ header.php
@@ -14,6 +14,14 @@
 <html <?php language_attributes(); ?>>
 <head>
 	
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M2P594T');</script>
+	<!-- End Google Tag Manager -->
+
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-60070578-14"></script>
 	<script>
@@ -35,6 +43,12 @@
 </head>
 
 <body <?php body_class(); ?>>
+	
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2P594T"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
 	<div id="app">
 		<div class="app-container">
 
@@ -51,7 +65,7 @@
 		<?php endif ?>
 		
 		<div class="container navbar-w100">
-			<nav class="navbar navbar-expand-md">
+			<nav class="navbar navbar-expand-md" id="navbar">
 				<div class="container" style="padding: 0;">
 
 					<a class="navbar-brand" href="<?php echo get_site_url() ?>"> 

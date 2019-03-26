69055afbc0c902b12f6fdab1df95015871b1586e
waone <waone.on1@gmail.com>
2019-03-26 10:48:43 +0700

    cta download on register section desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,29 @@
 
 } )(jQuery);
 
+// Tes
+( function($) {
+
+	// When the user scrolls the page, execute myFunction 
+	// window.onscroll = function() {myFunction()};
+
+	// Get the navbar
+	// var navbar = document.getElementById("navbar");
+	// var navbarm = document.getElementById("navbar-m");
+
+	// console.log(document.getElementById("navbar"));
+	// Get the offset position of the navbar
+	// var sticky = navbar.offsetTop;
+
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+	  // if (window.pageYOffset != sticky) {
+	    // navbar.classList.add("sticky");
+	    // navbarm.classList.add("sticky");
+	  // } else {
+	    // navbar.classList.remove("sticky");
+	    // navbarm.classList.remove("sticky");
+	  // }
+	}
+
+} )(jQuery);

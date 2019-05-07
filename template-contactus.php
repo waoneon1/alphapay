eae8a3551386e6ac184404e32ae7620442a378cd
waone <waone.on1@gmail.com>
2019-05-07 14:16:47 +0700

    kontak us

--- template-contactus.php
+++ template-contactus.php
@@ -46,23 +46,23 @@ get_header();
     <!-- Our Journey -->
     <div class="alp-pcontent alp-section">
     	<div class="bgbluebox"></div>
-    	<div class="container alp-padding">
+    	<div class="container alp-padding-top">
 	    	<div class="text-center">
 	    		<h2>Layanan Pelanggan</h2>
 	    		<p>Jika Anda mengalami kendala terkait transaksi di AlphaPay, Anda bisa langsung menghubungi layanan pelanggan (Customer Service) kami melalui:  </p>
 	    	</div>
 	        <div class="row">
 	           <div class="col-md-4 col-12">
-	               <div class="alp-boxnshadow">
+	               <div class="alp-boxnshadow alp-margin-bot">
 	                    <h4>
-	                    	<span class="dashicons alp-icon alp-icon-24"></span>
-	                    	Jam Operational
+	                    	<span class="dashicons alp-icon alp-icon-chat"></span>
+	                    	Live Chat
 	                	</h4>
-	                    <p>07.00 - 23.00 WIB</p>
+	                    <p>Tersedia di aplikasi.</p>
 	               </div>
 	           </div>
 	           <div class="col-md-4 col-12">
-	               <div class="alp-boxnshadow">
+	               <div class="alp-boxnshadow alp-margin-bot">
 	                    <h4>
 	                    	<span class="dashicons alp-icon alp-icon-email"></span>
 	                    	Email
@@ -71,12 +71,12 @@ get_header();
 	               </div>
 	           </div>
 	           <div class="col-md-4 col-12">
-	               <div class="alp-boxnshadow">
+	               <div class="alp-boxnshadow alp-margin-bot">
 	                    <h4>
-	                    	<span class="dashicons alp-icon alp-icon-chat"></span>
-	                    	Live Chat
+	                    	<span class="dashicons alp-icon alp-icon-24"></span>
+	                    	Jam Operational
 	                	</h4>
-	                    <p>Tersedia di aplikasi.</p>
+	                    <p>07.00 - 23.00 WIB</p>
 	               </div>
 	           </div>
 	        </div>
@@ -85,45 +85,56 @@ get_header();
     
 
     <!-- Follow -->
-    <div class="fullwidth-content">
+    <div class="fullwidth-content alp-follow-wrap">
 	    <div class="alp-subsc row alp-section alp-follow">
 	    	<div class="col-md-6 col-12 alp-subsc--left alp-row">
 	    		<div>
-	    			<h2>Follow kita di:</h2>
-	    			<div class="alp-subsc--form alp-form">
-	    				<div class="row">
-	    					<div class="col-10 alp-boxnshadow">
-		    					<div class="row">
-		    						<div class="col-md-4">
-						    			<span class="alp-roundshadow icon-fb">
-						    				<img class="svg" src="<?php echo get_template_directory_uri() ?>/assets/img/icon-fb.svg">
-						    			</span>
-		    						</div>
-		    						<div class="col-md-8" style="padding-top: 12px;"><span>alphapay.id</span></div>
+	    			<h2>Media Sosial Kami</h2>
+    				<div class="row">
+    					<div class="col-md-10 col-12">
+
+    						<a href="https://www.facebook.com/alphapay.id/" terget="_blank">
+	    						<div class="alp-boxnshadow">
+			    					<div class="row">
+			    						<div class="col-4">
+							    			<span class="alp-roundshadow icon-fb">
+							    				<img class="svg" src="<?php echo get_template_directory_uri() ?>/assets/img/icon-fb.svg">
+							    			</span>
+			    						</div>
+			    						<div class="col-8" style="padding-top: 12px;"><span>alphapay.id</span></div>
+			    					</div>
 		    					</div>
-		    				</div>
-		    				<div class="col-10 alp-boxnshadow">
-		    					<div class="row">
-		    						<div class="col-md-4">
-						    			<span class="alp-roundshadow icon-ig">
-											<img class="svg" src="<?php echo get_template_directory_uri() ?>/assets/img/icon-ig.svg">
-						    			</span>
-		    						</div>
-		    						<div class="col-md-8" style="padding-top: 12px;"><span>alphapay.id</span></div>
-		    					</div>
-		    				</div>
-		    				<div class="col-10 alp-boxnshadow">
-		    					<div class="row">
-		    						<div class="col-md-4">
-						    			<span class="alp-roundshadow icon-gm">
-											<img class="svg" src="<?php echo get_template_directory_uri() ?>/assets/img/icon-gm.svg">
-						    			</span>
-		    						</div>
-		    						<div class="col-md-8" style="padding-top: 12px;"><span>info@alphapay.id</span></div>
-		    					</div>
-		    				</div>
+	    					</a>
+
+	    					<a href="https://www.instagram.com/alphapay.id/" terget="_blank">
+			    				<div class="alp-boxnshadow">
+			    					<div class="row">
+			    						<div class="col-4">
+							    			<span class="alp-roundshadow icon-ig">
+												<img class="svg" src="<?php echo get_template_directory_uri() ?>/assets/img/icon-ig.svg">
+							    			</span>
+			    						</div>
+			    						<div class="col-8" style="padding-top: 12px;"><span>alphapay.id</span></div>
+			    					</div>
+			    				</div>
+			    			</a>
+
+			    			<a href="https://www.youtube.com/channel/UCwCI3-8ctmn14i_3TUTvd2w" terget="_blank">
+			    				<div class="alp-boxnshadow">
+			    					<div class="row">
+			    						<div class="col-4">
+							    			<span class="alp-roundshadow icon-gm">
+												<img class="svg" src="<?php echo get_template_directory_uri() ?>/assets/img/icon-yt.svg">
+							    			</span>
+			    						</div>
+			    						<div class="col-8" style="padding-top: 12px;"><span>AlphaPay Indonesia</span></div>
+			    					</div>
+			    				</div>
+			    			</a>
+
 	    				</div>
-	    			</div>
+	    				<div class="col-md-2 col-0"></div>
+    				</div>
 	    		</div>
 	    	</div>
 	    	<div class="col-md-6 col-12 alp-follow--right d-none d-md-block">

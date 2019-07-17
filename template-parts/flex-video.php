81c4f9454826d36d589f842979229ee0eb2cf15b
waone <waone.on1@gmail.com>
2019-07-18 00:20:25 +0700

    video embed auto resize

--- template-parts/flex-video.php
+++ template-parts/flex-video.php
@@ -6,13 +6,24 @@
 	        <p><?php echo $section['description'] ?></p> 
 	    <?php endif ?>
 	</div>
-	<?php echo $section['video']; ?>
+	<div class="alp-video-container">
+		<?php echo $section['video']; ?>	
+	</div>
 </div>
 
 
 <style type="text/css">
-	.alp-video iframe {
-		width: 100%;
-		height: 600px;
+	.alp-video-container {
+	    position: relative;
+	    width: 100%;
+	    height: 0;
+	    padding-bottom: 56.25%;
+	}
+	.alp-video-container iframe {
+	    position: absolute;
+	    top: 0;
+	    left: 0;
+	    width: 100%;
+	    height: 100%;
 	}
 </style>

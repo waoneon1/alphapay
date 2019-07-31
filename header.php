3a0a4b3fa6ca6ed2fa29d0a65f3599d740e19160
waone <waone.on1@gmail.com>
2019-07-31 14:20:27 +0700

    ads

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 	     (adsbygoogle = window.adsbygoogle || []).push({
-	          google_ad_client: "ca-pub-2156334577724467",
+	          google_ad_client: "ca-pub-7589434816280196",
 	          enable_page_level_ads: true
 	     });
 	</script>

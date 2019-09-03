6292f02d0cd9addf599dc052ae658069b00f4816
waone <waone.on1@gmail.com>
2019-09-03 15:16:05 +0700

    update style outlet profiling

--- functions.php
+++ functions.php
@@ -141,7 +141,7 @@ function alpay_scripts() {
 
 	wp_enqueue_style( 'alpay-main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.5' );
 
-	wp_enqueue_style( 'alpay-style', get_stylesheet_uri(), array(), '2.34'  );
+	wp_enqueue_style( 'alpay-style', get_stylesheet_uri(), array(), '2.4'  );
 
 	wp_enqueue_script( 'alpay-boostrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '20190101', true );
 

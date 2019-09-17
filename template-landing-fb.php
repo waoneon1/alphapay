efd8e37aa4e3d9ff2cb6b4e55465b7a94884d8dc
waone <waone.on1@gmail.com>
2019-09-17 17:57:54 +0700

    value decimal

--- template-landing-fb.php
+++ template-landing-fb.php
@@ -48,8 +48,8 @@
 
 
     <script>
-        $('.js-fbpixle').attr("onClick", "fbq('track', 'Purchase', {value: 5000, currency: 'IDR'});");
-        $('.js-fbpixle-custom').attr("onClick", "fbq('track', 'Purchase', {value: 5000, currency: 'IDR'});");
+        $('.js-fbpixle').attr("onClick", "fbq('track', 'Purchase', {value: 5000.00, currency: 'IDR'});");
+        $('.js-fbpixle-custom').attr("onClick", "fbq('track', 'Purchase', {value: 5000.00, currency: 'IDR'});");
     </script>
 
 </div>
